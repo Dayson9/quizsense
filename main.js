aec4c366995f7b6f2a15e966c19be32757a095e7
Dayson9 <ainsteinwells@gmail.com>
2024-05-08 13:45:06 +0100

    update

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 const {iRender, Signal} = QueFlow;
-var question = Signal("What is electolysis?");
+var question = Signal("What type of galaxy is the milky way?"), counter = Signal([0, 10]), option = Signal({a: "Irregular Galaxy", b: "Elliptical Galaxy", c: "Spiral Galaxy", d: "Circular Galaxy"});
 
 iRender("app");
