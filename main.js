0c9002b63e9a0be011b5f5784034d41ee116837a
Dayson9 <ainsteinwells@gmail.com>
2024-05-08 21:40:03 +0100

    update

--- main.js
+++ main.js
@@ -2,3 +2,9 @@ const {iRender, Signal} = QueFlow;
 var question = Signal("What type of galaxy is the milky way?"), counter = Signal([0, 10]), option = Signal({a: "Irregular Galaxy", b: "Elliptical Galaxy", c: "Spiral Galaxy", d: "Circular Galaxy"});
 
 iRender("app");
+
+const intro = document.getElementById("intro");
+
+const startQuiz = function(){
+intro.style.left = "-100vw";
+}
